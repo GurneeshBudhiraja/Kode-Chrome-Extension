0762a234715fe318811ede150f737a825d25ccd2
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2024-11-28 00:22:25 +0530

    feature: add the notes button in the leetcode taskbar

--- src/content.js
+++ src/content.js
@@ -17,3 +17,22 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   return true;
 });
 
+const ele = document.querySelector(
+  '.flexlayout__tabset_tabbar_inner_tab_container_top'
+);
+console.log('ELEMENT IS : ');
+console.log(ele);
+
+const newElement = document.createElement('div');
+newElement.textContent = 'Notes';
+newElement.style.color = 'red';
+newElement.style.fontSize = '20px';
+newElement.style.zIndex = '100';
+newElement.addEventListener('mousedown', () => {
+  console.log('Notes Clicked');
+});
+
+ele.appendChild(newElement);
+console.log(
+  document.querySelector('.flexlayout__tabset_tabbar_inner_tab_container_top')
+);
